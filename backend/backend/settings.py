9e46801f742d7d60c5b41eb6b189d837b24062c4
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2023-10-22 15:56:13 -0500

    testing deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/var/www/car_dealership_backend/static'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
