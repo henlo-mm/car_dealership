77f082ac7ffa74f81c9a3d5f1ea67066198c15db
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2023-11-04 16:37:01 -0500

    Actualización de archivos estáticos

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = '/var/www/car_dealership/backend/static'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
