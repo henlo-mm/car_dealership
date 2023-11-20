8d12f922b3143718b13975bf1d1898ec4be008c3
Sebastian Rey <reysebastian77@gmail.com>
2023-11-20 10:18:27 -0500

    ajuste error en apið

--- front-React/src/services/quotes.jsx
+++ front-React/src/services/quotes.jsx
@@ -1,9 +1,6 @@
 import api from '../api/config';
 
-<<<<<<< HEAD
-=======
-// http://3.223.190.219/quotes/
->>>>>>> 8ee618749caf4a63d1915692e08d7457bbcc3e13
+
 
 export const getQuotes = async () => {
     try {
